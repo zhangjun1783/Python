6060c8a8590314b464346b07c6f67376c03490c1
zhangjun <zhangjun1783@163.com>
2024-12-06 10:43:10 +0800

    Default Changelist

--- test12.py
+++ test12.py
@@ -4,9 +4,9 @@ from selenium import webdriver
 import os
 
 url='http://www.baidu.com'
-iedriver ='C:\Program Files (x86)\Python37-32\IEDriverServer.exe' #iedriver路径
+#iedriver ='C:\Program Files (x86)\Python37-32\IEDriverServer.exe' #iedriver路径
 os.environ["webdriver.ie.driver"] = iedriver #设置环境变量
 driver = webdriver.Ie(iedriver)
 driver.get(url)
 driver.close()
-#11222  
+#2024-12-10
